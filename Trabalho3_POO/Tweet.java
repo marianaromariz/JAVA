b7b9866d1510826d7962be6a6ba3dcdbe0024723
Mariana Romariz <marianaromariz@outlook.com>
2019-09-08 17:19:21 -0300

    Update Tweet.java

--- Trabalho3_POO/Tweet.java
+++ Trabalho3_POO/Tweet.java
@@ -3,24 +3,29 @@
 
 //classe Tweet
 public class Tweet{
+	
 	//define variável para armazenar o tweet
 	private String desc;
-	//verificando tamanho do tweet
+	
+	//verificando o tamanho do tweet
 	public Tweet(String msg){
 		if (msg.length() >= 140){
 			//se for maior que 140 caracteres, trunca o tweet em 140 caracteres e o armazena
 			msg = msg.substring(0, 140);
 		}
+		//senão
 		else{
-			//armazena o tweet na variável msg
+			//apenas armazena o tweet na variável msg
 			this.desc = msg;
 		}
 	}
-	//criando método para acesso
+	
+	//criando método para acesso da variável "desc" da classe Tweet
 	public String getTweet(){
 		return this.desc;
 	}
-	//criando método para modificação
+	
+	//criando método para modificação da variável "desc" da classe Tweet
 	public void setTweet(String msg){
 		this.desc = msg;
 	}
