0fe9b03400158613dbcb5a59d897726aad69c66f
Mariana Romariz <marianaromariz@outlook.com>
2019-09-08 17:07:45 -0300

    Update Usuario.java

--- Trabalho3_POO/Usuario.java
+++ Trabalho3_POO/Usuario.java
@@ -1,21 +1,26 @@
+//Mariana Fernandes Campos Romariz
+//0050008393
+
 import java.util.ArrayList;
 
+//classe Usuario
 public class Usuario {
-
+	//definição de variáveis do tipo string para armazenar usuário e senha
 	private String u;
 	private String s;
 	
+	//definição de vetor para armazenar os tweets utilizando a classe Tweet
 	private ArrayList<Tweet> tts;
-	
+	//construtor da classe Usuario
 	public Usuario(String u, String s){
-		// TODO Auto-generated constructor stub
+		// instancia de vetor pra classe Tweet
 		this.tts = new ArrayList<>();
-		
+		//armazenando usuário e senha
 		this.setU(u);
 		this.setS(s);
-		
+		//verificando valor da senha
 		if (!s.equalsIgnoreCase("poo")) { 
-			System.out.println("Senha incorreta");
+			System.out.println("ACESSO NEGADO! SENHA INCORRETA!");
 			System.exit(0);
 		}
 	}
@@ -36,14 +41,18 @@ public class Usuario {
 		this.s = s;
 	}
 	
+	//metodo para criar tweets
 	public void tweeta(String msg){
+		//instancia da classe Tweet
 		Tweet tt = new Tweet(msg);
+		//criando tweet
 		this.tts.add(tt);
 	}
 
 	public void listatts(){
+		//imprime o usuário logado
 		System.out.printf("\nUsuário: %s\n", this.getU());
-
+		//enquanto houver tweets, imprime os tweets
 		for (int i = 0; i < this.tts.size(); i++){
 			Tweet tt = this.tts.get(i);
 			System.out.printf("TT %s:" , i+1 );
