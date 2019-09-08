97affce0ed81b84cde7ce8796088a6d2fc9f89d3
Mariana Romariz <marianaromariz@outlook.com>
2019-09-08 17:01:10 -0300

    Update Tweet.java

--- Trabalho3_POO/Tweet.java
+++ Trabalho3_POO/Tweet.java
@@ -1,9 +1,13 @@
+//Mariana Fernandes Campos Romariz
+//0050008393
 
+//classe Tweet
 public class Tweet{
 	private String desc;
-	
+	//verificando tamanho do tweet
 	public Tweet(String msg){
 		if (msg.length() >= 140){
+			//truncando a mensagemem 140 caracteres
 			msg = msg.substring(0, 140);
 		}
 		else{
